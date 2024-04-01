49be4759fd58200b060861e780921384fbf0ac37
Aila Manlangit <aila.janelle@yahoo.com>
2024-04-01 21:41:52 +0800

    Added comparison and analysis documentation

--- MCO2/lbyarch_mco2/main.c
+++ MCO2/lbyarch_mco2/main.c
@@ -6,6 +6,14 @@ extern double sdotfunc(double f1, double f2, long long int n, double* ASMsdot);
 extern double sdotInC(double f1, double f2, long long n, double* ASMsdot);
 
 int main() {
+	int asm_pass = 0;
+	int c_pass = 0;
+	double c_time30[30];
+	double asm_time30[30];
+	double c_time24[30];
+	double asm_time24[30];
+	double c_time20[30];
+	double asm_time20[30];
 	int loope = 30;
 	long long int n;
 	int i;
@@ -32,13 +40,16 @@ int main() {
 		ansC = sdotInC(f1, f2, n, &ASMsdot);
 		QueryPerformanceCounter(&li);
 		end = li.QuadPart;
+		c_time30[i] = ((double)(end - start)) * 1000.00 / PCFreq;
 		elapseC += ((double)(end - start)) * 1000.00 / PCFreq;
 		totalElapseC += elapseC;
 
-		if (ansC == 1073741824)
+		/*if (ansC == 1073741824)
 			printf("C function 2^30 Pass %d\n", i + 1);
 		else
-			printf("C function 2^30 Fail %d\n", i + 1);
+			printf("C function 2^30 Fail %d\n", i + 1);*/
+		if (ansC == 1073741824)
+			c_pass++;
 
 		// time for asm function
 		QueryPerformanceCounter(&li);
@@ -46,22 +57,39 @@ int main() {
 		ansAsm = sdotfunc(f1, f2, n, &ASMsdot);
 		QueryPerformanceCounter(&li);
 		end = li.QuadPart;
+		asm_time30[i] = ((double)(end - start)) * 1000.00 / PCFreq;
 		elapseAsm += ((double)(end - start)) * 1000.00 / PCFreq;
 		totalElapseAsm += elapseAsm;
 
+		//if (ansAsm == 1073741824)
+			//printf("ASM function 2^30 Pass %d\n", i + 1);
 		if (ansAsm == 1073741824)
-			printf("ASM function 2^30 Pass %d\n", i + 1);
-		else
-			printf("ASM function 2^30 Fail %d\n", i + 1);
+			asm_pass++;
 
-		printf("\n");
+		//printf("\n");
 	}
 
+
 	printf("----------\n");
-	printf("Average time for C function in 2^30: %lf ms\n", elapseC / loope);
-	printf("Average time for x86-64 function in 2^30: %lf ms\n", elapseAsm / loope);
+	//UNCOMMENT TO SEE THE TIME FOR EACH LOOP
+	/*printf("C function in 2^30 time: [");
+	for (int i = 0; i < 30; i++) {
+		printf("%.4lf ", c_time30[i]);
+	}
+	printf("]\n");
+	printf("ASM function in 2^30 time: [");
+	for (int i = 0; i < 30; i++) {
+		printf("%.4lf ", asm_time30[i]);
+	}
+	printf("]\n\n"); */
+	printf("C function in 2^30 finished with average time: %lf ms\n", elapseC / loope);
+	printf("C function in 2^30 finished with %d%% CORRECTNESS\n", (c_pass / 30)*100);
+	printf("ASM function in 2^30 finished with average time: %lf ms\n", elapseAsm / loope);
+	printf("ASM function in 2^30 finished with %d%% CORRECTNESS\n", (asm_pass / 30)*100);
 	printf("----------\n\n");
 
+	c_pass = 0;
+	asm_pass = 0;
 	for (i = 0; i < loope; i++) {
 		n = 16777216;
 		double f1 = 1.0;
@@ -76,13 +104,16 @@ int main() {
 		ansC = sdotInC(f1, f2, n, &ASMsdot);
 		QueryPerformanceCounter(&li);
 		end = li.QuadPart;
+		c_time24[i] = ((double)(end - start)) * 1000.00 / PCFreq;
 		elapseC += ((double)(end - start)) * 1000.00 / PCFreq;
 		totalElapseC += elapseC;
 
-		if (ansC == 16777216)
+		/*if (ansC == 16777216)
 			printf("C function 2^24 Pass %d\n", i + 1);
 		else
-			printf("C function 2^24 Fail %d\n", i + 1);
+			printf("C function 2^24 Fail %d\n", i + 1);*/
+		if (ansC == 16777216)
+			c_pass++;
 
 		// time for asm function
 		QueryPerformanceCounter(&li);
@@ -90,22 +121,40 @@ int main() {
 		ansAsm = sdotfunc(f1, f2, n, &ASMsdot);
 		QueryPerformanceCounter(&li);
 		end = li.QuadPart;
+		asm_time24[i] = ((double)(end - start)) * 1000.00 / PCFreq;
 		elapseAsm += ((double)(end - start)) * 1000.00 / PCFreq;
 		totalElapseAsm += elapseAsm;
 
+		//if (ansAsm == 16777216)
+		//	printf("ASM function 2^24 Pass %d\n", i + 1);
+		//else
+		//	printf("ASM function 2^24 Fail %d\n", i + 1);
 		if (ansAsm == 16777216)
-			printf("ASM function 2^24 Pass %d\n", i + 1);
-		else
-			printf("ASM function 2^24 Fail %d\n", i + 1);
+			asm_pass++;
 
-		printf("\n");
+		//printf("\n");
 	}
 
 	printf("----------\n");
-	printf("Average time for C function in 2^24: %lf ms\n", elapseC / loope);
-	printf("Average time for x86-64 function in 2^24: %lf ms\n", elapseAsm / loope);
+	//UNCOMMENT TO SEE THE TIME FOR EACH LOOP
+	/*printf("C function in 2^24 time: [");
+	for (int i = 0; i < 30; i++) {
+		printf("%.4lf ", c_time24[i]);
+	}
+	printf("]\n");
+	printf("ASM function in 2^24 time: [");
+	for (int i = 0; i < 30; i++) {
+		printf("%.4lf ", asm_time24[i]);
+	}
+	printf("]\n\n");*/
+	printf("C function in 2^24 finished with average time: %lf ms\n", elapseC / loope);
+	printf("C function in 2^24 finished with %d%% CORRECTNESS\n", (c_pass / 30) * 100);
+	printf("ASM function in 2^24 finished with average time: %lf ms\n", elapseAsm / loope);
+	printf("ASM function in 2^24 finished with %d%% CORRECTNESS\n", (asm_pass / 30) * 100);
 	printf("----------\n\n");
 
+	asm_pass = 0;
+	c_pass = 0;
 	for (i = 0; i < loope; i++) {
 		n = 1048576;
 		double f1 = 1.0;
@@ -120,13 +169,16 @@ int main() {
 		ansC = sdotInC(f1, f2, n, &ASMsdot);
 		QueryPerformanceCounter(&li);
 		end = li.QuadPart;
+		c_time20[i] = ((double)(end - start)) * 1000.00 / PCFreq;
 		elapseC += ((double)(end - start)) * 1000.00 / PCFreq;
 		totalElapseC += elapseC;
 
+		//if (ansC == 1048576)
+		//	printf("C function 2^20 Pass %d\n", i+1);
+		//else
+		//	printf("C function 2^20 Fail %d\n", i+1);
 		if (ansC == 1048576)
-			printf("C function 2^20 Pass %d\n", i+1);
-		else
-			printf("C function 2^20 Fail %d\n", i+1);
+			c_pass++;
 
 		// time for asm function
 		QueryPerformanceCounter(&li);
@@ -134,20 +186,36 @@ int main() {
 		ansAsm = sdotfunc(f1, f2, n, &ASMsdot);
 		QueryPerformanceCounter(&li);
 		end = li.QuadPart;
+		asm_time20[i] = ((double)(end - start)) * 1000.00 / PCFreq;
 		elapseAsm += ((double)(end - start)) * 1000.00 / PCFreq;
 		totalElapseAsm += elapseAsm;
 
+		//if (ansAsm == 1048576)
+		//	printf("ASM function 2^20 Pass %d\n", i+1);
+		//else
+		//	printf("ASM function 2^20 Fail %d\n", i+1);
 		if (ansAsm == 1048576)
-			printf("ASM function 2^20 Pass %d\n", i+1);
-		else
-			printf("ASM function 2^20 Fail %d\n", i+1);
+			asm_pass++;
 
-		printf("\n");
+		//printf("\n");
 	}
 
 	printf("----------\n");
-	printf("Average time for C function in 2^20: %lf ms\n", elapseC / loope);
-	printf("Average time for x86-64 function in 2^20: %lf ms\n", elapseAsm / loope);
+	//UNCOMMENT TO SEE THE TIME FOR EACH LOOP
+	/*printf("C function in 2^20 time: [");
+	for (int i = 0; i < 30; i++) {
+		printf("%.4lf ", c_time20[i]);
+	}
+	printf("]\n");
+	printf("ASM function in 2^20 time: [");
+	for (int i = 0; i < 30; i++) {
+		printf("%.4lf ", asm_time20[i]);
+	}
+	printf("]\n\n");*/
+	printf("C function in 2^20 finished with average time: %lf ms\n", elapseC / loope);
+	printf("C function in 2^20 finished with %d%% CORRECTNESS\n", (c_pass / 30) * 100);
+	printf("ASM function in 2^20 finished with average time: %lf ms\n", elapseAsm / loope);
+	printf("ASM function in 2^20 finished with %d%% CORRECTNESS\n", (asm_pass / 30) * 100);
 	printf("----------\n\n");
 
 	printf("----------------------------------------\n");
--- README.md
+++ README.md
@@ -1,8 +1,40 @@
 # LBYARCH
 Source code for MCO Projects in LBYARCH T2 AY 2023-2024
 
-Please refer to MCO2/lbyarch_mco2 for MCO2 files
+Please refer to [MCO2/lbyarch_mco2](https://github.com/jiiometrii/LBYARCH/tree/main/MCO2/lbyarch_mco2) for MCO2 files
+
+## Table of Contents
+- [I.) Execution time comparison and analysis of kernel performance of C vs x86_64 assembly](#i-execution-time-comparison-and-analysis-of-kernel-performance-of-c-vs-x86_64-assembly)
+    - [i.) Execution time comparison of kernel performance of C vs x86_64 assembly, w/o individual values](#ii-execution-time-comparison-of-kernel-performance-of-c-vs-x86_64-assembly-w-o-individual-values)
+    - [ii.) Execution time comparison of kernel performance of C vs x86_64 assembly, w individual values](#ii-execution-time-comparison-of-kernel-performance-of-c-vs-x86_64-assembly-w-individual-values)
+    - [iii.) Comparison and analysis of kernel performance using Test of Significance](#iii-comparison-and-analysis-of-kernel-performance-using-test-of-significance)
+    - [iv.) Discussion](#iv-discussion)
+- [II.) Program output with correctness check of C & x86_64 assembly](#ii-program-output-with-correctness-check-of-c-x86_64-assembly)
 
 ## I.) Execution time comparison and analysis of kernel performance of C vs x86_64 assembly
-## II.) Program output with correctness check of C
-## III.) Program output with correctness check of C
+### i.) Execution time comparison of kernel performance of C vs x86_64 assembly, w/o individual values
+<div align="center">
+    <img src="image.png" alt="execution-time-noindiv">
+</div>
+
+### ii.) Execution time comparison of kernel performance of C vs x86_64 assembly, w individual values
+<div align="center">
+    <img src="image-1.png" alt="execution-time-indiv">
+</div>
+
+### iii.) Comparison and analysis of kernel performance using Test of Significance
+<div align="center">
+    <img src="image-2.png" alt="stat-test-comp30">
+    <p style="font-size: larger;"><strong>Comparison for 2^30</strong></p>
+    <img src="image-3.png" alt="stat-test-comp30">
+    <p style="font-size: larger;"><strong>Comparison for 2^24</strong></p>
+    <img src="image-4.png" alt="stat-test-comp30">
+    <p style="font-size: larger;"><strong>Comparison for 2^20</strong></p>
+</div>
+
+### iv.) Discussion
+We can see from the values itself in section i. and ii. that the average execution time of x86_64 assembly is faster than C. In running the functions in the size 2^30, x86_64 assembly was almost 5x faster than C. For 2^24 and 2^20 vector sizes, it's approximately 4x faster. We decided to apply the Test of Significance to see if there is statistically a significance in kernel performance. With reference to the images in section iii, the Test of Significance statistically shows us that x86_64 assembly is significantly faster than C.
+## II.) Program output with correctness check of C & x86_64 assembly
+<div align="center">
+    <img src="image-5.png" alt="correctness-check">
+</div>
